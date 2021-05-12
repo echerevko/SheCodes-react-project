04fef52d99c6eca530f10e80d72956625d6017a8
echerevko <43856956+echerevko@users.noreply.github.com>
2021-05-12 12:04:26 +0200

    added prettier

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-Hello here    </div>
+      <Weather />
+      Hello here
+    </div>
   );
 }
 
