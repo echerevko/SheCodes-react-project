bd0fa7104c8a9a8dc89bad0dc332c2114771bafa
echerevko <43856956+echerevko@users.noreply.github.com>
2021-05-26 16:25:23 +0200

    implemented weather forecast for 6 days, used useEffect

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -44,7 +44,7 @@ export default function Weather(props) {
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city"
+                placeholder="Enter a city.."
                 className="form-control"
                 autoFocus="on"
                 onChange={handleCityChange}
@@ -53,7 +53,7 @@ export default function Weather(props) {
             <div className="col-3">
               <input
                 type="submit"
-                valuer="Search"
+                value="Search"
                 className="btn btn-primary w-100"
               />
             </div>
@@ -65,6 +65,6 @@ export default function Weather(props) {
     );
   } else {
     search();
-    return "Loading..";
+    return "Loading.";
   }
 }
