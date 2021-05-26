5cfe6c8d4e612ecd7c58aed1eed5623b67c156c4
echerevko <43856956+echerevko@users.noreply.github.com>
2021-05-26 16:43:21 +0200

    refactored the cod

--- weather-app/src/WeatherForecast.js
+++ weather-app/src/WeatherForecast.js
@@ -15,6 +15,16 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+
+  function load() {
+    let apiKey = "41b994c32cd18a931e3e8c1b0b2c94c9";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -34,11 +44,6 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "41b994c32cd18a931e3e8c1b0b2c94c9";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    return null;
+    load();
   }
 }
