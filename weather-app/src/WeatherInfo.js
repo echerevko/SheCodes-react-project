9f514032fd97ca4b39547a2b30c992ba83b6e535
echerevko <43856956+echerevko@users.noreply.github.com>
2021-05-26 15:53:18 +0200

    updated format of daily data

--- weather-app/src/WeatherInfo.js
+++ weather-app/src/WeatherInfo.js
@@ -18,8 +18,7 @@ export default function WeatherInfo(props) {
           <div className="d-flex">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
+            </div>{" "}
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
